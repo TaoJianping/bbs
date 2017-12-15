f28acb6e7dda8c2da7714af9fd78e9472df92fcd
Tao <tjp563613470@live.com>
2017-12-15 21:46:21 +0800

    一些小小的修改

--- routes/reply.py
+++ routes/reply.py
@@ -10,15 +10,9 @@ from flask import (
 
 from models.user import User
 from models.reply import Reply
-from .bbs import login_require
+from .bbs import login_require, current_user
 
 main = Blueprint("reply", __name__)
-
-
-def current_user():
-    username = session.get("username", None)
-    user = User.find_by(username=username)
-    return user
 
 
 @main.route("/add", methods=["POST"])
--- routes/topic.py
+++ routes/topic.py
@@ -27,7 +27,7 @@ main = Blueprint("topic", __name__)
 @login_require
 def topic_write():
     """
-    发布新话题的文章页面，用来写新话题
+    返回发布新话题的文章页面
     """
     boards = Board.find_all()
     return render_template("BBS/topic_write.html", boards=boards)
@@ -37,7 +37,7 @@ def topic_write():
 @login_require
 def markdown_upload():
     log(request.form)
-    return redirect(url_for('.markdown'))
+    return redirect(url_for('.detail'))
 
 
 @main.route("/<topic_id>")
@@ -50,9 +50,11 @@ def detail(topic_id):
 
 
 @main.route("/add", methods=["POST"])
+@login_require
 def add():
     form = request.form
     user = current_user()
+    log("add函数这边的User是：", user)
     Topic.new(form, user_id=user._id)
     return redirect(url_for("topic.index"))
 
